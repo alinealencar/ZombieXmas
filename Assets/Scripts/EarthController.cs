350ca65431a346a1086d7ca42d8914746774a609
Aline <aline.nevesalencar@georgebrown.ca>
2017-10-12 19:39:13 -0400

    First commit

---
+++ Assets/Scripts/EarthController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthController : MonoBehaviour {
+
+	// Public variables
+	[SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float startX;
+	[SerializeField]
+	private float endX;
+	// Private variables
+	private Transform _transform;
+	private Vector2 _curPosition;
+
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform> ();
+		_curPosition = _transform.position;
+
+		Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_curPosition = _transform.position;
+		// Move the background from the right to the left
+		_curPosition -= new Vector2 (speed, 0);
+
+		if (_curPosition.x < endX) {
+			Reset ();
+		}
+
+		_transform.position = _curPosition;
+	}
+
+	private void Reset(){
+		_curPosition = new Vector2 (startX, 0);
+	}
+}
---
+++ Assets/Scripts/SnowballController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 7f;
+	[SerializeField]
+	private float upperBound; 
+	[SerializeField]
+	private float lowerBound;
+
+	private Transform _transform;
+	private Vector2 _curPosition;
+
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform> ();
+		_curPosition = _transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_curPosition = _transform.position;
+		// Move Mr. Snowball
+		if(Input.GetKey(KeyCode.W)){
+			// Press A, move him upwards
+			_curPosition += new Vector2(0, speed);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			// Press S, move him downwards
+			_curPosition -= new Vector2(0, speed);
+		}
+		CheckBounds ();
+		_transform.position = _curPosition;
+	}
+
+	private void CheckBounds(){
+		if (_curPosition.y < lowerBound) {
+			_curPosition.y = lowerBound;
+		}
+
+		if (_curPosition.y > upperBound) {
+			_curPosition.y = upperBound;
+		}
+	}
+
+}
